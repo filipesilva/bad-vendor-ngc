89c67afd343fbd0992e5fbeff63998f172ce7223
Filipe Silva <filipematossilva@gmail.com>
2017-11-02 17:21:09 +0000

    add angular2-click-outside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ClickOutsideDirective } from 'angular2-click-outside/clickOutside.directive';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClickOutsideDirective
   ],
   imports: [
     BrowserModule
